efb9d9103f2f847a32f719b8b58b98d7a29f69d3
J Rhee <jayrhee0514@gmail.com>
2021-02-01 14:34:42 -0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './App.css';
-import Navbar from "../src/components/Navbar"
+import Navbar from "./components/Navbar"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Home from "./components/pages/Home"
 import Portfolios from "./components/pages/Portfolios";
