c76476756d185e55991fff9ff639d50b3753cfd7
J Rhee <jayrhee0514@gmail.com>
2021-04-29 11:13:55 -0400

    fixed a typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
                         JR <i class="fas fa-bahai" /> 
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
-                        <i classNane={click ? "fas fa-times" : "fas fa-bars"} />
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
                     </div>
                     <ul className ={click ? "nav-menu active" : "nav-menu"}>
                         <li className="nav-item">
